1bc4a9b00b164548b473c8d5ba1ba6814aa08a39
Naive Roboticist <naiveroboticist@gmail>
2014-09-21 09:46:12 -0600

    Cleaned and refactored main window.

--- lib/cursed_console/main_window.rb
+++ lib/cursed_console/main_window.rb
@@ -9,15 +9,17 @@ module CursedConsole
 
     def initialize(plugin_manager, web_service_client)
       super(0, 0, 0, 0)
+
       @current_position = -1
       @plugin_manager = plugin_manager
       @web_service_client = web_service_client
       @menu_list = @plugin_manager.plugins
+
       Curses::curs_set(0)
       color_set(1)
       keypad(true)
+
       render_menu
-      write_status_message 
     end
 
     def main_loop
@@ -28,6 +30,7 @@ module CursedConsole
         when Curses::Key::LEFT
           self.current_position -= 1
         when 13, Curses::Key::ENTER
+          next if current_position < 0 # Don't do anything if nothing selected
           return current_position if current_position == menu_list.size
           submenu_select = render_sub_menu(current_position)
           if submenu_select.present?
@@ -35,7 +38,6 @@ module CursedConsole
             invoke_action(plugin_manager.plugins[current_position], 
                           submenu_select.first, 
                           submenu_select.last)
-            # return [ current_position ] + submenu_select
           end
         else
           next if ch.is_a?(Fixnum)
@@ -43,10 +45,11 @@ module CursedConsole
           selected_item = mlist.detect { |item| item.downcase.start_with?(ch.downcase) }
           self.current_position = mlist.index(selected_item) unless selected_item.nil?
         end
+        
         self.current_position = 0 if current_position < 0
         self.current_position = menu_list.size if current_position > menu_list.size
+
         render_menu(self.current_position)
-        update_status_bar
       end
     end
 
@@ -66,43 +69,23 @@ module CursedConsole
       end
     end
     
-    def update_status_bar
-      # write_status_message(menu_list[menu_list[current_position]]) unless current_position < 0
-    end
-
-    def write_status_message(message=nil, offset=0)
-      # %x{ echo "Line: #{lines - 1}, Message: #{message}" >> log.txt}
-      # Clear the status line
-      setpos(maxy - 1, 0)
-      attron(Curses::A_STANDOUT)
-      addstr(" " * maxx)
-      attroff(Curses::A_STANDOUT)
-
-      if ! message.nil?
-        setpos(maxy - 1, 1)
-        attron(Curses::A_STANDOUT)
-        addstr(message)
-        attroff(Curses::A_STANDOUT)
-      end
-    end
-
     def position_for_submenu(selected_menu_item)
-      pos = 2
-      plugin_manager.plugins.each_with_index do | item, index |
-        break if selected_menu_item >= index
+      pos = 1
+      menu_list.each_with_index do | item, index |
+        break if selected_menu_item <= index
         pos += (item.length + 1)
       end
       pos
     end
 
     def render_sub_menu(position)
-      sub_path = plugin_manager.plugins[position]
-      plugins = plugin_manager.resources_for(sub_path)
-      plugin_menu = plugins.map do | plugin |
-        { id: plugin, display: plugin.capitalize }
+      plugin = menu_list[position]
+      resources = plugin_manager.resources_for(plugin)
+      resource_menu = resources.map do | resource |
+        { id: resource, display: resource.capitalize }
       end
-      submenu = DropDownMenu.new(plugin_menu,
-                                 sub_path,
+      submenu = DropDownMenu.new(resource_menu,
+                                 plugin,
                                  plugin_manager,
                                  1, 
                                  position_for_submenu(position))
@@ -113,42 +96,37 @@ module CursedConsole
       end
     end
 
-    def invoke_action(sub_path, plugin, action)
-      # Instantiate the plugin...
-      plugin = plugin_manager.instantiate_resource(sub_path, plugin)
-      if plugin.requires_input_for?(action.to_sym)
-        form = PluginForm.new(plugin, action.to_sym, self, 20, 80, 2, 2)
-        form.handle_form(web_service_client)
-        form.clear
-        form.refresh
-        form.close
+    def invoke_action(plugin, resource, action)
+      # Instantiate the resource...
+      rsrc = plugin_manager.instantiate_resource(plugin, resource)
+      if rsrc.requires_input_for?(action.to_sym)
+        handle_form(rsrc, action)
       else
         # No input required. Don't need no stinkin' form
-        the_form = plugin.send(action.to_sym)
-        uri = the_form[:result]
-        list = plugin.send(the_form[:result_formatter], web_service_client, uri, {})
-        data_list = DataList.new(list)
-        #submenu = DropDownMenu.new(list, 
-                                  #nil, # sub_path
-                                  #nil, # plugin_manager
-                                  #1,
-                                  #1,
-                                  #self)  # status_bar
-        selected = data_list.handle_list.tap do | selection |
-          data_list.clear
-          data_list.refresh
-          data_list.close
-          Curses::curs_set(1)
-        end
-        #selected = submenu.select_menu_item.tap do | selection |
-          #submenu.clear
-          #submenu.refresh
-          #submenu.close
-          #Curses::curs_set(1)
-          ## render_fields
-        #end
-        refresh
+        handle_results(rsrc, action)
       end
+    end
+
+    def handle_form(rsrc, action)
+      form = PluginForm.new(rsrc, action.to_sym, self, 20, 80, 2, 2)
+      form.handle_form(web_service_client)
+      form.clear
+      form.refresh
+      form.close
+    end
+
+    def handle_results(rsrc, action)
+      the_form = rsrc.send(action.to_sym)
+      uri = the_form[:result]
+      list = rsrc.send(the_form[:result_formatter], web_service_client, uri, {})
+      data_list = DataList.new(list)
+      selected = data_list.handle_list.tap do | selection |
+        data_list.clear
+        data_list.refresh
+        data_list.close
+        Curses::curs_set(1)
+      end
+      refresh
     end
 
   end
